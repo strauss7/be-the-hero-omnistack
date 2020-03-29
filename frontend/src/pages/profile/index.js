70bc2a95fc7528c8a8250c74c4af69e70b2fa322
strauss7 <wbsabbyyuji@gmail.com>
2020-03-29 11:51:06 -0300

    mobile app

--- frontend/src/pages/profile/index.js
+++ frontend/src/pages/profile/index.js
@@ -33,7 +33,7 @@ export default function Profile() {
                 }
             });
 
-            setIncidents(incidents.filter(incident => incident.id !== id));
+            setIncidents(incidents.filter(incident => incident.id != id));
         } catch (err) {
             alert('Erro ao deletar o caso, tente novamente.')
         }
